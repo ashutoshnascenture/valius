235583f579f7d754e9bbec490f0f886cfecb7ba3
shoppaldev <ashutosh@nascenture.com>
2018-11-19 15:45:26 +0530

    invoice

--- resources/views/sites/singlesitedetail.blade.php
+++ resources/views/sites/singlesitedetail.blade.php
@@ -18,18 +18,18 @@
 		<div class="row nav-box">
 			<div class="col-md-9 left-nav">
 				<nav class="navbar navbar-expand-lg">
-					<ul class="navbar-nav">
+					<ul class="navbar-nav nav-pills">
 						<li class="nav-item active">
-							<a class="nav-link" href="#">Overview <span class="sr-only">(current)</span></a>
+							<a  data-toggle="pill" class="nav-link"  href="#menu1">Overview <span class="sr-only">(current)</span></a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">Billing</a>
+							<a data-toggle="pill"  class="nav-link"  href="#menu2">Billing</a>
 						</li>
 					</ul>
 				</nav>
 			</div>
 			<div class="col-md-3 right-nav">
-				<div class="dropdown">
+				<!-- <div class="dropdown">
 					<a class="dropdown-toggle" data-toggle="dropdown">
 						<i class="fa fa-cog" aria-hidden="true"></i>
 					</a>
@@ -38,15 +38,31 @@
 						<a class="dropdown-item" href="#">Link 2</a>
 						<a class="dropdown-item" href="#">Link 3</a>
 					</div>
-				</div>
+				</div> -->
 				<a href="#" class="btn btn-admin"> wp admin </a>
 			</div>
 		</div>
 	</div>
 </section> 
+<!-- <section class="section mt-4">
+	<div class="container tab-pane fade" id="menu1">
+		<div class="row " >
+			<div class="col-md-12">
+			<div class="card custome-card">
+				<div class="card-header">
+					Overview
+				</div>
+				<div class="card-body">
+					Overview
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+</section> -->
 <section class="section mt-4">
-	<div class="container">
-		<div class="row">
+	<div class="container tab-pane" id="menu2">
+		<div class="row " >
 			<div class="col-md-12">
 			<div class="card custome-card">
 				<div class="card-header">
@@ -80,10 +96,4 @@
 </section>
 	</div>
 </section>
-
-
-
-
-
-
 @endsection
